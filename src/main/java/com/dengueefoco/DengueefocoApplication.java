9d8fca1c1995718db4ca30f8baf947d23fade85c
Christian Tusset <christian.tusset@projuris.com.br>
2019-10-12 22:28:30 -0300

    Removido imports desnecessários

--- src/main/java/com/dengueefoco/DengueefocoApplication.java
+++ src/main/java/com/dengueefoco/DengueefocoApplication.java
@@ -1,11 +1,7 @@
 package com.dengueefoco;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class DengueefocoApplication {
